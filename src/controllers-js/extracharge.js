cae973648c334cc5da24d247d24bc1eecdc28d2a
vaibhav_jogdand <vrjogdand708@gmail.com>
2022-05-04 12:03:49 +0530

    Controllers_change

--- src/controllers-js/extracharge.js
+++ src/controllers-js/extracharge.js
@@ -1,72 +1,72 @@
 const Extracharge = require("../models-js/extracharge");
 
 exports.getAllExtraCharges = (req, res) => {
-  Extracharge.find({})
-    .then((response) => {
-      if (response) {
-        res.status(200).json(response);
-      } else return res.status(404).json({ message: "ExtraCharge not found" });
-    })
-    .catch((error) => {
-      return res.status(404).json({ message: "Internal Server Error" });
-    });
+	Extracharge.find({})
+		.then((response) => {
+			if (response) {
+				res.status(200).json(response);
+			} else return res.status(404).json({ message: "ExtraCharge not found" });
+		})
+		.catch((error) => {
+			return res.status(404).json({ message: "Internal Server Error" });
+		});
 };
 
 exports.saveExtraChargeById = (req, res, next, id) => {
-  Extracharge.findById(id)
-    .then((response) => {
-      if (response) {
-        req.extraCharge = response;
-        next();
-      } else return res.status(404).json({ message: "ExtraCharge not found" });
-    })
-    .catch((error) => {
-      return res.status(404).json({ message: "Internal Server Error" });
-    });
+	Extracharge.findById(id)
+		.then((response) => {
+			if (response) {
+				req.extraCharge = response;
+				next();
+			} else return res.status(404).json({ message: "ExtraCharge not found" });
+		})
+		.catch((error) => {
+			return res.status(404).json({ message: "Internal Server Error" });
+		});
 };
 
 exports.getExtraChargeById = (req, res) => {
-  return res.status(200).json(req.extraCharge);
+	return res.status(200).json(req.extraCharge);
 };
 
 exports.createExtraCharge = (req, res) => {
-  const extraCharge = new Extracharge(req.body);
-  extraCharge
-    .save()
-    .then((response) => {
-      if (response) return res.status(200).json(response);
+	const extraCharge = new Extracharge(req.body.data);
+	extraCharge
+		.save()
+		.then((response) => {
+			if (response) return res.status(200).json(response);
 
-      return res.status(404).json({ message: "ExtraCharge creation failed" });
-    })
-    .catch((error) => {
-      return res.status(404).json({ message: "Internal Server Error" });
-    });
+			return res.status(404).json({ message: "ExtraCharge creation failed" });
+		})
+		.catch((error) => {
+			return res.status(404).json({ message: "Internal Server Error" });
+		});
 };
 
 exports.updateExtraCharge = (req, res) => {
-  Extracharge.findByIdAndUpdate(
-    req.extraCharge._id,
-    { $set: req.body },
-    { new: true, useFindAndModify: false }
-  )
-    .then((response) => {
-      if (response) return res.status(200).json(response);
+	Extracharge.findByIdAndUpdate(
+		req.params.extraCharge,
+		{ $set: req.body.data },
+		{ new: true, useFindAndModify: false }
+	)
+		.then((response) => {
+			if (response) return res.status(200).json(response);
 
-      return res.status(404).json({ message: "ExtraCharge updation failed" });
-    })
-    .catch((error) => {
-      return res.status(404).json({ message: "Internal Server Error" });
-    });
+			return res.status(404).json({ message: "ExtraCharge updation failed" });
+		})
+		.catch((error) => {
+			return res.status(404).json({ message: "Internal Server Error" });
+		});
 };
 
 exports.deleteExtraCharge = (req, res) => {
-  Extracharge.findByIdAndDelete(req.extraCharge._id)
-    .then((response) => {
-      if (response) return res.status(200).json(response);
-      else
-        return res.status(500).json({ message: "ExtraCharge deletion failed" });
-    })
-    .catch((error) => {
-      return res.status(500).json({ message: "Internal Server Error" });
-    });
+	Extracharge.findByIdAndDelete(req.params.extraCharge)
+		.then((response) => {
+			if (response) return res.status(200).json(response);
+			else
+				return res.status(500).json({ message: "ExtraCharge deletion failed" });
+		})
+		.catch((error) => {
+			return res.status(500).json({ message: "Internal Server Error" });
+		});
 };
--- src/models-js/driver.js
+++ src/models-js/driver.js
@@ -5,6 +5,8 @@ const driverSchema = new Schema(
 		name: String,
 		phoneNo: String,
 		salary: String,
+		aadhar: String,
+		aadharCard: { data: Buffer, contentType: String },
 	},
 	{ timestamps: true }
 );
