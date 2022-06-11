59e70320d209d84ceb298e78797b1c40212b5ded
omkargodham <omkargodham321@gmail.com>
2022-06-11 17:35:40 +0530

    salaryDetails added to the driver

--- src/models-js/driver.js
+++ src/models-js/driver.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const driverSchema = new Schema(
-	{
-		name: String,
-		phoneNo: String,
-		salary: String,
-		aadhar: String,
-		trips: [],
-		aadharCard: { data: Buffer, contentType: String },
-		chargePerTrip: String,
-	},
-	{ timestamps: true }
+  {
+    name: String,
+    phoneNo: String,
+    salary: String,
+    aadhar: String,
+    trips: [],
+    aadharCard: { data: Buffer, contentType: String },
+    chargePerTrip: String,
+    salaryDetails: [], //[{month,advance,remaining,currentSalary}]
+  },
+  { timestamps: true }
 );
 module.exports = mongoose.model("Driver", driverSchema);
