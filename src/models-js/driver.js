0cde744f25475c8e080b6b7d3a725127a193d57a
vaibhav_jogdand <vrjogdand708@gmail.com>
2022-06-15 11:45:33 +0530

    changes

--- src/models-js/driver.js
+++ src/models-js/driver.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const driverSchema = new Schema(
-  {
-    name: String,
-    phoneNo: String,
-    salary: String,
-    aadhar: String,
-    trips: [],
-    aadharCard: { data: Buffer, contentType: String },
-    chargePerTrip: String,
-    salaryDetails: [], //[{month,advance,remaining,currentSalary}]
-  },
-  { timestamps: true }
+	{
+		name: String,
+		phoneNo: String,
+		salary: String,
+		aadhar: String,
+		trips: [],
+		aadharCard: { data: Buffer, contentType: String },
+		license: { data: Buffer, contentType: String },
+		chargePerTrip: String,
+		salaryDetails: [], //[{month,advance,remaining,currentSalary}]
+	},
+	{ timestamps: true }
 );
 module.exports = mongoose.model("Driver", driverSchema);
