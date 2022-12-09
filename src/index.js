ee0d8ea3854d64d7d4d6667a2c820dcfcf09d539
omkargodham <omkargodham321@gmail.com>
2022-12-09 13:09:14 +0530

    checking the render host

--- src/index.js
+++ src/index.js
@@ -8,13 +8,15 @@ app.use(cors());
 // app.use(bodyParser.json());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(
-	bodyParser.urlencoded({
-		limit: "50mb",
-		extended: true,
-		parameterLimit: 50000,
-	})
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
 );
-
+app.get("/hello", (req, res) => {
+  res.json({ name: "Hello Swapnil" });
+});
 app.use("/users", require("./routes-js/user"));
 app.use("/trip", require("./routes-js/trip"));
 app.use("/customer", require("./routes-js/customer"));
@@ -27,26 +29,26 @@ app.use("/bill", require("./routes-js/bill"));
 
 // set headers
 app.use((req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader(
-		"Access-Control-Allow-Methods",
-		"GET, POST, PUT, PATCH, DELETE"
-	);
-	next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  next();
 });
 
 mongoose
-	.connect(process.env.MONGODB_CONNECT, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
-	.then(() => {
-		console.log("MongoDB Connnected");
-	})
-	.catch((err) => {
-		console.log(err, "Error Occured Connecting To DB");
-	});
-
-app.listen(process.env.PORT || 3003, () => {
-	console.log("server running on port 3003", process.env.PORT);
+  .connect(process.env.MONGODB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connnected");
+  })
+  .catch((err) => {
+    console.log(err, "Error Occured Connecting To DB");
+  });
+const port = process.env.PORT || 3003;
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
 });
